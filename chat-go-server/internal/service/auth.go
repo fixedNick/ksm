026fd3887989348af7ff933fccf23b0d44bcbd14
fixedNick <kkos44911@gmail.com>
2026-07-25 23:26:27 +0300

    interfaces update, auth/chat service implementation

--- chat-go-server/internal/service/auth.go
+++ chat-go-server/internal/service/auth.go
@@ -1,3 +1,101 @@
 package service
 
-type AuthService struct{}
+import (
+	"context"
+	"fmt"
+	"ksm-chat/internal/domain"
+	"ksm-chat/internal/infrastructure/jwt"
+	"ksm-chat/internal/infrastructure/limits"
+	"strings"
+)
+
+type AuthService struct {
+	userRepository IUserRepository
+	hasher         IHasherComparer
+	jwtService     ITokenGenerator
+}
+
+func NewAuthService(userRepo IUserRepository, hasher IHasherComparer, jwtService ITokenGenerator) *AuthService {
+	return &AuthService{
+		userRepository: userRepo,
+		hasher:         hasher,
+		jwtService:     jwtService,
+	}
+}
+
+func (as *AuthService) SignUp(ctx context.Context, username, plainPassword string) (*domain.User, *domain.Token, error) {
+	// validate is username and passwords valid
+	if err := validateCredentials(username, plainPassword); err != nil {
+		return nil, nil, err
+	}
+
+	// hash password
+	pwdHash, err := as.hasher.Hash(plainPassword)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Create User Account
+	user, err := as.userRepository.Create(ctx, username, pwdHash)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Generate JWT
+	token, err := as.jwtService.Generate(user.Username(), user.ID(), string(jwt.ISSUER_AUTH))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return user, token, nil
+}
+func (as *AuthService) SignIn(ctx context.Context, username, plainPassword string) (*domain.User, *domain.Token, error) {
+	// Check is input valid
+	if err := validateCredentials(username, plainPassword); err != nil {
+		return nil, nil, err
+	}
+
+	user, err := as.userRepository.GetByUsername(ctx, username)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Compare passwords
+	if err := as.hasher.Compare(plainPassword, user.PasswordHash()); err != nil {
+		return nil, nil, err
+	}
+
+	// Passwords matched
+	// Generate JWT token
+	token, err := as.jwtService.Generate(username, user.ID(), string(jwt.ISSUER_AUTH))
+	return user, token, err
+}
+func (as *AuthService) UpdateTokens(ctx context.Context, refreshToken string) (*domain.Token, error) {
+	panic("not implemented")
+}
+
+func validateCredentials(username string, plainPassword string) error {
+	if len(username) > limits.USERNAME_MAX_LENGTH || len(username) < limits.USERNAME_MIN_LENGTH {
+		return fmt.Errorf("username must be in range [> %d and < %d]", limits.USERNAME_MIN_LENGTH, limits.USERNAME_MAX_LENGTH)
+	}
+
+	if len(plainPassword) > limits.PASSWORD_MAX_LENGTH || len(plainPassword) < limits.PASSWORD_MIN_LENGTH {
+		return fmt.Errorf("password must be in range [> %d and < %d]", limits.PASSWORD_MIN_LENGTH, limits.PASSWORD_MAX_LENGTH)
+	}
+
+	for _, symbolsRange := range limits.PASSWORD_REQUIRE_SYMBOLS {
+		symbolFound := false
+		for _, symbol := range symbolsRange {
+			if strings.ContainsRune(plainPassword, symbol) {
+				symbolFound = true
+				break
+			}
+		}
+
+		if !symbolFound {
+			return fmt.Errorf("password must contain one of the symbols: [%s]", symbolsRange)
+		}
+	}
+
+	return nil
+}
--- chat-go-server/internal/service/interfaces.go
+++ chat-go-server/internal/service/interfaces.go
@@ -8,16 +8,24 @@ import (
 	"github.com/google/uuid"
 )
 
-type ChatRepository interface {
+type IChatRepository interface {
 	Create(ctx context.Context, owner uuid.UUID, chatName string, chatType domain.ChatType) (*domain.Chat, error)
 	Delete(ctx context.Context, chatID uuid.UUID) error
 	Rename(ctx context.Context, chatID uuid.UUID, newName string) error
+
 	GetChatsByUserID(ctx context.Context, userID uuid.UUID) ([]*domain.Chat, error)
 	GetChatsWithMembersByUserID(ctx context.Context, userID uuid.UUID) ([]*domain.ChatWithMembers, error)
+
 	GetChatByID(ctx context.Context, chatID uuid.UUID) (*domain.Chat, error)
+	GetChatWithMembersByID(ctx context.Context, chatID uuid.UUID) (*domain.ChatWithMembers, error)
+
+	AddMember(ctx context.Context, chatID uuid.UUID, targetUserID uuid.UUID) error
+	RemoveMember(ctx context.Context, chatID uuid.UUID, targetUserID uuid.UUID) error
+
+	IsUserInChat(ctx context.Context, chatID uuid.UUID, targetUserID uuid.UUID) (bool, error)
 }
 
-type MessageRepository interface {
+type IMessageRepository interface {
 	Save(ctx context.Context, chatID, senderID uuid.UUID, content string, attachments []string, createdAt time.Time) (*domain.Message, error)
 	EditMessage(ctx context.Context, messageID uint64, content string, attachments []string) (*domain.EditedMessage, error)
 	GetHistory(ctx context.Context, chatID uuid.UUID, limit int, beforeMessageID uint64) ([]*domain.Message, error)
@@ -28,40 +36,53 @@ type MessageRepository interface {
 	GetReadStatus(ctx context.Context, messageID uint64) ([]uuid.UUID, error)
 }
 
-type UserRepository interface {
+type IUserRepository interface {
 	Create(ctx context.Context, username, pwdHash string) (*domain.User, error)
 	GetByID(ctx context.Context, id uuid.UUID) (*domain.User, error)
 	GetByUsername(ctx context.Context, username string) (*domain.User, error)
 }
 
-type PresenceRepository interface {
+type ITokenGenerator interface {
+	Generate(username string, userId uuid.UUID, issuer string) (*domain.Token, error)
+}
+
+type ITokenValidator interface {
+	Validate(tokenStr string) (username string, userId string, err error)
+}
+
+type IHasherComparer interface {
+	Hash(plainPassword string) (string, error)
+	Compare(plainPasswrod, dbHash string) error
+}
+
+type IPresenceRepository interface {
 	SetOnline(ctx context.Context, userID uuid.UUID, ttl time.Duration) error
 	SetOffline(ctx context.Context, userID uuid.UUID) error
 	GetOnlineStatus(ctx context.Context, userIDs []uuid.UUID) (map[uuid.UUID]bool, error)
 }
 
-type MessageBroker interface {
+type IMessageBroker interface {
 	Publish(ctx context.Context, channel string, msg *domain.Message) error
 	Subscribe(ctx context.Context, channel string) (<-chan *domain.Message, error)
 }
 
 type IAuthService interface {
-	SignUp(ctx context.Context, username, plainPassword string) (*domain.User, error)
+	SignUp(ctx context.Context, username, plainPassword string) (*domain.User, *domain.Token, error)
 	SignIn(ctx context.Context, username, plainPassword string) (*domain.User, *domain.Token, error)
-	RefreshToken(ctx context.Context, refreshToken string) (*domain.Token, error)
+	UpdateTokens(ctx context.Context, refreshToken string) (*domain.Token, error)
 }
 
 type IChatService interface {
-	GetUserChats(ctx context.Context, userID uuid.UUID) ([]*domain.Chat, error)
-	GetUserChatsWithMembers(ctx context.Context, userID uuid.UUID) ([]*domain.ChatWithMembers, error)
+	GetUserChats(ctx context.Context, requestFromUserID, targetUserID uuid.UUID) ([]*domain.Chat, error)
+	GetUserChatsWithMembers(ctx context.Context, requestFromUserID, targetUserID uuid.UUID) ([]*domain.ChatWithMembers, error)
 
 	CreatePersonalChat(ctx context.Context, ownerID, targetUserID uuid.UUID) (*domain.Chat, error)
-	CreateGroupChat(ctx context.Context, ownerID uuid.UUID, chatType domain.ChatType, membersToAdd []uuid.UUID) (*domain.Chat, error)
+	CreateGroupChat(ctx context.Context, ownerID uuid.UUID, chatName string, chatType domain.ChatType, membersToAdd []uuid.UUID) (*domain.Chat, error)
 
-	AddMember(ctx context.Context, chatID uuid.UUID, targetUserID uuid.UUID) error
-	RemoveMember(ctx context.Context, chatID uuid.UUID, targetUserID uuid.UUID) error
+	AddMember(ctx context.Context, chatID uuid.UUID, requestFromUserID, targetUserID uuid.UUID) error
+	RemoveMember(ctx context.Context, chatID uuid.UUID, requestFromUserID, targetUserID uuid.UUID) error
 
-	ChangeChatName(ctx context.Context, chatID uuid.UUID, newName string) (*domain.Chat, error)
+	ChangeChatName(ctx context.Context, chatID, requestFromUserID uuid.UUID, newName string) (*domain.Chat, error)
 }
 
 type IMessageService interface {
